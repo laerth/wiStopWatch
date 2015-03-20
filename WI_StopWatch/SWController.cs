78ce4b2957f55e069a980410edd40a06d665a194
laerth <solo-d@mail.ru>
2015-03-20 22:29:42 +0600

    cc

---
+++ WI_StopWatch/SWController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WI_StopWatch
+{
+    class SWController : IStopWatch
+    {
+        private StopWatch stopWatch;
+        private frmMain frm;
+        private Thread updater;
+
+        public SWController(frmMain form)
+        {
+            frm = form;
+            stopWatch = new StopWatch();
+        }
+
+        private void UpdateLabel()
+        {
+            while (true)
+            {
+                frm.ChangeText(stopWatch.GetTime().ToString(@"hh\:mm\:ss"));
+                Thread.Sleep(250);
+            }
+        }
+
+        public void Start()
+        {
+            updater = new Thread(UpdateLabel);
+            updater.Start();
+            stopWatch.Start();
+        }
+
+        public void Stop()
+        {
+            stopWatch.Stop();
+        }
+
+        public void Reset()
+        {
+            stopWatch.Reset();
+        }
+
+        public TimeSpan GetTime()
+        {
+            return stopWatch.GetTime();
+        }
+    }
+}
