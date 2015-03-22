f27d3627ba95c679d554882ccd72f7f4849562af
laerth <solo-d@mail.ru>
2015-03-22 19:59:56 +0600

    list2

---
+++ WI_StopWatch/Controls/UI_SWList.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WI_StopWatch
+{
+    partial class UI_SWList
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panButtons = new System.Windows.Forms.Panel();
+            this.tbCaption = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.panControls = new System.Windows.Forms.Panel();
+            this.panButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panButtons
+            // 
+            this.panButtons.Controls.Add(this.tbCaption);
+            this.panButtons.Controls.Add(this.btnAdd);
+            this.panButtons.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panButtons.Location = new System.Drawing.Point(0, 0);
+            this.panButtons.Name = "panButtons";
+            this.panButtons.Size = new System.Drawing.Size(287, 30);
+            this.panButtons.TabIndex = 0;
+            // 
+            // tbCaption
+            // 
+            this.tbCaption.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCaption.Location = new System.Drawing.Point(3, 7);
+            this.tbCaption.Name = "tbCaption";
+            this.tbCaption.Size = new System.Drawing.Size(248, 20);
+            this.tbCaption.TabIndex = 2;
+            this.tbCaption.Text = "...";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnAdd.Location = new System.Drawing.Point(257, 0);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(30, 30);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "a";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // panControls
+            // 
+            this.panControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panControls.Location = new System.Drawing.Point(0, 30);
+            this.panControls.Name = "panControls";
+            this.panControls.Size = new System.Drawing.Size(287, 222);
+            this.panControls.TabIndex = 1;
+            // 
+            // UI_SWList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panControls);
+            this.Controls.Add(this.panButtons);
+            this.Name = "UI_SWList";
+            this.Size = new System.Drawing.Size(287, 252);
+            this.panButtons.ResumeLayout(false);
+            this.panButtons.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panButtons;
+        private System.Windows.Forms.TextBox tbCaption;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Panel panControls;
+    }
+}
---
+++ WI_StopWatch/Controls/UI_SWList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WI_StopWatch
+{
+    public partial class UI_SWList : UserControl
+    {
+        private List<UserControl> controls;
+
+        public UI_SWList()
+        {
+            InitializeComponent();
+
+            controls = new List<UserControl>();
+        }
+
+        private void AddControl()
+        {
+            UIStopWatch control = new UIStopWatch(tbCaption.Text);
+            control.Dock = DockStyle.Top;
+            controls.Add(new UIStopWatch(tbCaption.Text));
+            panControls.Controls.Add(control);
+            control.Start();
+        }
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddControl();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
